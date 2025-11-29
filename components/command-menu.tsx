f8c48ce524c0d6fb598d320a908d070422731b56
Eden Alencar <eden.alencar@outlook.com>
2025-11-29 14:37:36 -0300

    fix: improve light mode contrast for dialogs and search button

--- components/command-menu.tsx
+++ components/command-menu.tsx
@@ -42,7 +42,7 @@ export function CommandMenu() {
         <>
             <Button
                 variant="outline"
-                className="relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2"
+                className="relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2 bg-muted/50 text-muted-foreground"
                 onClick={() => setOpen(true)}
             >
                 <Search className="h-4 w-4 xl:mr-2" />
